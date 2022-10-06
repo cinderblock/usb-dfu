d9e28a9d4736a509c98d5fa1d7731c3577fb947b
Cameron Tacklind <cameron@tacklind.com>
2022-10-05 17:50:36 -0700

    Different versions of gcc produce significantly different sized binaries

---
+++ local.Noook.mk
@@ -0,0 +1,21 @@
+
+# Different versions of GCC produce different sized outputs...
+
+GCC_Version=avr-gcc-6.1.0-x86-mingw# 1117
+GCC_Version=avr-gcc-6.1.0-x64-mingw# 1117
+GCC_Version=avr-gcc-7.1.0-x86-mingw# 1081
+GCC_Version=avr-gcc-7.1.0-x64-mingw# 1081
+GCC_Version=avr-gcc-7.2.0-x86-mingw# 1081
+GCC_Version=avr-gcc-7.2.0-x64-mingw# 1081
+GCC_Version=avr-gcc-7.3.0-x64-mingw# 1081
+GCC_Version=avr-gcc-8.3.0-x64-mingw# 1087
+GCC_Version=avr-gcc-9.2.0-x64-mingw# 1385
+GCC_Version=avr-gcc-10.1.0-x64-windows# 1395
+GCC_Version=avr-gcc-11.1.0-x64-windows# 1395
+GCC_Version=avr-gcc-11.1.0-x86-windows# 1395
+GCC_Version=avr-gcc-12.1.0-x64-windows# 1437
+GCC_Version=avr8-gnu-toolchain-win32_x86_64# "overflow by 30 bytes"
+
+GCC_Version=avr8-gnu-toolchain-win32_x86
+
+COMPILER_PATH="C:\Program Files\avr-gcc\$(GCC_Version)\bin/"
