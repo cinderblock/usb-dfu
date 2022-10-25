fa36753dceea36f16966b0fc9865820da7fdbfe9
Cameron Tacklind <cameron@tacklind.com>
2022-10-25 14:08:08 -0700

    Use old version on Quasar

--- local.Quasar.mk
+++ local.Quasar.mk
@@ -1,1 +1,5 @@
-COMPILER_PATH = "C:/Program Files/avr-gcc-11.1.0-x64-windows/bin/"
+COMPILER_PATH = "C:/Program Files/avr-gcc/avr-gcc-11.1.0-x64-windows/bin/"
+
+GCC_Version=avr8-gnu-toolchain-win32_x86
+
+COMPILER_PATH="C:/Program Files/avr-gcc/$(GCC_Version)/bin/"
